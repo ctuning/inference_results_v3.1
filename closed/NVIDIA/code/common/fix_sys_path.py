908bca47e5ae876654124c5586157330acdff479
Arjun <arjunsuresh1987@gmail.com>
2023-09-14 23:22:35 +0000

    Fixes for harness build

--- closed/NVIDIA/code/common/fix_sys_path.py
+++ closed/NVIDIA/code/common/fix_sys_path.py
@@ -50,7 +50,7 @@ def _is_userhome_local(path):
     while path != os.path.sep:
         path, head = os.path.split(path)
         path_dirs.append(head)
-    return len(path_dirs) >= 4 and path_dirs[-1] == "home" and path_dirs[-3] == ".local"
+    return False#len(path_dirs) >= 4 and path_dirs[-1] == "home" and path_dirs[-3] == ".local"
 
 
 # Remove duplicates and maintain ordering
